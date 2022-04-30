e3cfeaeeebc51e7ccf8a70810fc1629a5b0e8fb9
Berezkina <berezkinaaa@gmail.com>
2022-04-30 21:07:24 +0700

    настройка сборки

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	entry: './src/main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build'),
-		clean: true,
-	},
-	devtool: 'source-map',
-	plugins: [
-		new CopyPlugin({
-			patterns: [{ from: 'public' }],
-		}),
-	],
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
 };
